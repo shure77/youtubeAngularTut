4ed14f7486d60b1c2da230c4ef7dcd7e0aa3adb1
Thaler <Roman.Thaler@adesso.at>
2019-07-25 07:31:58 +0200

    Project started

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { CarsComponent } from './cars/cars.component';
+import { ReportComponent } from './report/report.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    CarsComponent,
+    ReportComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/engine.spec.ts
@@ -0,0 +1,7 @@
+import { Engine } from './models/engine';
+
+describe('Engine', () => {
+  it('should create an instance', () => {
+    expect(new Engine()).toBeTruthy();
+  });
+});
