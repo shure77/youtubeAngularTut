11af60b0a29e719f53ecc62475960d92f91ac75e
Thaler <Roman.Thaler@adesso.at>
2019-07-26 07:50:11 +0200

    adding bootstrap example template, adding car and engin models

--- src/app/models/car.ts
+++ src/app/models/car.ts
@@ -1,2 +1,12 @@
+import { Engine } from './engine';
+
 export class Car {
+
+    private brand: string;
+    private model: string;
+    private fabYear: number;
+    private price: number;
+    private km: number;
+    engine: Engine;
+
 }
--- src/app/models/engine.ts
+++ src/app/models/engine.ts
@@ -1,2 +1,7 @@
 export class Engine {
+
+    private fuelType: 'petrol' | 'diesel' | 'electric';
+    private power: number;
+    private fuelConsumption: number;
+
 }
